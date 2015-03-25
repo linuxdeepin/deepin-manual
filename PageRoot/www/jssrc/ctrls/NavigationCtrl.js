f35b06114c20631ea8b7b7d3ab943c94487c24e0
Xinkai <chenxinkai@linuxdeepin.com>
2015-03-25 16:13:10 +0800

    fix sideNavBarStyle not recalc.

--- PageRoot/www/jssrc/ctrls/NavigationCtrl.js
+++ PageRoot/www/jssrc/ctrls/NavigationCtrl.js
@@ -1,16 +1,16 @@
 "use strict";
 
-var navigationCtrl = angular.module("DManual")
-    .controller("NavigationBarCtrl", function($scope, $log) {
-        var sideNavItems = document.getElementById("SideNavigationItems");
+angular.module("DManual")
+    .controller("NavigationBarCtrl", function($scope, $log, $window) {
+        let sideNavItems = document.getElementById("SideNavigationItems");
         angular.element(sideNavItems).on("wheel", function(event) {
             sideNavItems.scrollTop -= event.wheelDeltaY;
         });
 
         // auto-resize
-        var container = document.getElementById("Container");
-        var logoBox = document.getElementById("NavLogoBox");
-        var updateSidebar = function() {
+        let container = document.getElementById("Container");
+        let logoBox = document.getElementById("NavLogoBox");
+        let updateSidebar = function() {
             let newHeight = container.clientHeight - logoBox.clientHeight;
             $log.log("resizing to " + newHeight + "px");
             $scope.sideNavBarStyle = {
@@ -18,7 +18,7 @@ var navigationCtrl = angular.module("DManual")
             };
         };
         updateSidebar();
-        angular.element(container).bind("resize", function(event) {
+        angular.element($window).bind("resize", function(event) {
             updateSidebar();
             $scope.$apply();
         });
