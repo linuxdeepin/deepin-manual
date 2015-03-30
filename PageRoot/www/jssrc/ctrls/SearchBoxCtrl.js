3f69370772bac4f82bdaaab731045664635d8eb9
Xinkai <chenxinkai@linuxdeepin.com>
2015-03-30 11:09:25 +0800

    autocomplete

--- PageRoot/www/jssrc/ctrls/SearchBoxCtrl.js
+++ PageRoot/www/jssrc/ctrls/SearchBoxCtrl.js
@@ -1,12 +1,13 @@
 "use strict";
 
 angular.module("DManual")
-       .controller("SearchBoxCtrl", function($scope, $rootScope) {
+       .controller("SearchBoxCtrl", function($scope, $rootScope, $log) {
             $scope.headers = [];
             $scope.$watch("searchTerm", function(newValue, oldValue){
                 $rootScope.$broadcast("searchTermChanged", newValue);
             });
             $scope.$on("headersSet", function(event, value) {
+                $log.log("Headers changed to", value);
                 $scope.headers = value;
             });
        });
