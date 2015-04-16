35dbf7c54c6760d326f2f5827bd1e4fe67ac89b0
Xinkai <chenxinkai@linuxdeepin.com>
2015-04-16 13:41:34 +0800

    remove old tests for getDManFileInfo

--- PageRoot/spec/path_spec.js
+++ PageRoot/spec/path_spec.js
@@ -2,69 +2,6 @@
 
 var utils = require("../www/jssrc/utils");
 var expect = require("expect.js");
-var gdmfi = utils.getDManFileInfo;
-
-describe("Resolution: DMan scheme", function() {
-    it("Dman scheme", function() {
-        let p = "dman://app2#Intro";
-        expect(gdmfi(p, "en_US")).to.eql({
-            baseDir: "file:///usr/share/dman/app2",
-            dir: "file:///usr/share/dman/app2/en_US",
-            hash: "Intro",
-            lang: "en_US",
-        });
-    });
-
-    it("Dman scheme with language code in the url", function() {
-        let p = "dman://app2?lang=en_US#Conclusion";
-        expect(gdmfi(p)).to.eql({
-            baseDir: "file:///usr/share/dman/app2",
-            dir: "file:///usr/share/dman/app2/en_US",
-            hash: "Conclusion",
-            lang: "en_US",
-        });
-    });
-
-    it("File scheme(explicit)", function() {
-        let p = "file:///home/user/Desktop/work/app3_enUS.md";
-        expect(gdmfi(p)).to.eql({
-            baseDir: "file:///home/user/Desktop/work",
-            dir: "file:///home/user/Desktop/work",
-            hash: null,
-            lang: null,
-        })
-    });
-
-    it("File scheme(implicit)", function() {
-        let p = "/home/user/Desktop/work/app2_enUS.md";
-        expect(gdmfi(p)).to.eql({
-            baseDir: "file:///home/user/Desktop/work",
-            dir: "file:///home/user/Desktop/work",
-            hash: null,
-            lang: null,
-        });
-    });
-
-    it("HTTP scheme", function() {
-        let p = "http://docs.example.com/dman/app3/zhCN/main.md";
-        expect(gdmfi(p)).to.eql({
-            baseDir: "http://docs.example.com/dman/app3",
-            dir: "http://docs.example.com/dman/app3/zhCN",
-            hash: null,
-            lang: "zhCN",
-        });
-    });
-
-    it("HTTPS scheme", function() {
-        let p = "https://docs.example.com/dman/app3/zhCN/main.md#使用说明";
-        expect(gdmfi(p)).to.eql({
-            baseDir: "https://docs.example.com/dman/app3",
-            dir: "https://docs.example.com/dman/app3/zhCN",
-            hash: "使用说明",
-            lang: "zhCN",
-        });
-    });
-});
 
 
 describe("Other Paths", function() {
