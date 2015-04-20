3cef1b6509a10ea03d6fe789e6e12697329b4b81
Xinkai <chenxinkai@linuxdeepin.com>
2015-04-20 14:12:22 +0800

    commit python files for DAE

---
+++ DMan/main_dae.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3 -OO
+# -*- coding: utf-8 -*-
+
+from dae.daeclient import DAEClient
+import os, sys
+
+if __name__ == "__main__":
+    path = os.path.normpath(os.path.dirname(__file__) + "/../PageRoot/www")
+    DAEClient(path)
