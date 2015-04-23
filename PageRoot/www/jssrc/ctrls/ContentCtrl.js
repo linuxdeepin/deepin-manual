e8e4b5dbffd608ee380eef551eab865886a6f3d9
Xinkai <chenxinkai@linuxdeepin.com>
2015-04-23 17:34:31 +0800

    better handling SpaceHolderResize

--- PageRoot/www/jssrc/ctrls/ContentCtrl.js
+++ PageRoot/www/jssrc/ctrls/ContentCtrl.js
@@ -4,16 +4,26 @@ angular.module("DManual")
     .controller("ContentCtrl", function($scope, $log, $window) {
         // Resize the block at the end of the content
         let iframe = document.getElementById("Content");
-        $window.addEventListener("resize", function() {
+        let resizeSpaceHolder = function() {
+            let iframeHeight = iframe.getBoundingClientRect().height;
+            if (iframeHeight === 0) {
+                return;
+            }
             let offsets = $scope.anchorsOffsetList;
             let lastOffset = offsets[offsets.length - 1];
             let iframeDoc = iframe.contentDocument;
             let footer = iframeDoc.querySelector("footer");
-            let h = iframe.getBoundingClientRect().height
-                    - footer.offsetTop
-                    + lastOffset
-                    - parseInt($window.getComputedStyle(iframe)["padding-top"]);
-            $log.log(`Set Content Footer to ${h}px`);
-            footer.style.height = `${h}px`;
-        });
+            let h = iframeHeight
+                - footer.offsetTop
+                + lastOffset
+                - parseInt($window.getComputedStyle(iframe)["padding-top"]);
+            if (h >= 0) {
+                $log.log(`Set Content Footer to ${h}px`);
+                footer.style.height = `${h}px`;
+            } else {
+                $log.log(`Set Content Footer to 0px`);
+                footer.style.height = `0px`;
+            }
+        };
+        $window.addEventListener("resize", resizeSpaceHolder);
     });
