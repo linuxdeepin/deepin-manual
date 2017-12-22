54b3a9a3be2533f7188b7597f27b97bf1be4010b
Hualet Wang <mr.asianwang@gmail.com>
2017-12-22 11:56:56 +0800

    remove locale setting since qcef has it handled
    Change-Id: Ibabf359f279975fe8c54224d2575fc2ce095ae22

--- src/app/dman.cpp
+++ src/app/dman.cpp
@@ -46,10 +46,6 @@ int main(int argc, char** argv) {
   // Disable GPU process.
   settings.addCommandLineSwitch("--disable-gpu", "");
 
-  // Follow system locale
-  QString sysLocale( QLocale::system().name() );
-  settings.addCommandLineSwitch("--lang", sysLocale.replace("_", "-"));
-
   // TODO(Shaohua): Set web cache folder.
 
   const int exit_code = QCefInit(argc, argv, settings);
