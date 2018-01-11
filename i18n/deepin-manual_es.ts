73bcc55b89f0c07dcf29af229fdeb00d0d033869
transifex <transifex@linuxdeepin.com>
2018-01-11 11:28:29 +0000

    auto sync po files from transifex
    Change-Id: I432edc5bed5180dcbb15db20b43ebf661b8b14a0

---
+++ i18n/deepin-manual_es.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/app/dman.cpp" line="80"/>
+        <source>Deepin Manual is designed to help users learn deepin and Deepin applications, providing specific instructions and function descriptions.</source>
+        <translation>Deepin Manual está diseñado para ayudar a los usuarios a aprender deepin y aplicaciones Deepin, proporcionando instrucciones específicas y descripciones de funciones.</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="30"/>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="31"/>
+        <source>System</source>
+        <translation>Sistema</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="32"/>
+        <source>Applications</source>
+        <translation>Aplicaciones</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="34"/>
+        <source>Sorry, there are no search results of &quot;%1&quot;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="35"/>
+        <source>Change your keywords and try again, or search it in Deepin Wiki</source>
+        <translation>Cambie sus palabras clave y vuelva a intentarlo, o búsquelo en Deepin Wiki</translation>
+    </message>
+    <message>
+        <location filename="../src/view/widget/search_completion_window.cpp" line="111"/>
+        <location filename="../src/view/widget/search_completion_window.cpp" line="154"/>
+        <source>Search &quot;%1&quot; in the full text</source>
+        <translation>Busque &quot;%1&quot; en el texto completo</translation>
+    </message>
+    <message>
+        <location filename="../src/view/widget/title_bar.cpp" line="89"/>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+</context>
+</TS>
