20eab8a887668290fe7d6facc18b2050489e4958
xushaohua <xushaohua@deepin.com>
2018-01-15 15:25:30 +0800

    Open url with default browser
    Change-Id: I4f0740998c31f9dd2d3b4eed43b63a6db4fc5ab9

--- src/view/web_event_delegate.cpp
+++ src/view/web_event_delegate.cpp
@@ -17,6 +17,8 @@
 
 #include "view/web_event_delegate.h"
 
+#include <QDesktopServices>
+
 namespace dman {
 
 WebEventDelegate::WebEventDelegate(QObject* parent) : QObject(parent) {
@@ -39,6 +41,10 @@ void WebEventDelegate::onBeforeContextMenu(QCefWebPage* web_page,
 
 bool WebEventDelegate::onBeforePopup(const QUrl& url,
                                      QCefWindowOpenDisposition disposition) {
+  if (disposition == QCefWindowOpenDisposition::QCEF_WOD_NEW_BACKGROUND_TAB ||
+      disposition == QCefWindowOpenDisposition::QCEF_WOD_NEW_FOREGROUND_TAB) {
+    QDesktopServices::openUrl(url);
+  }
   return QCefBrowserEventDelegate::onBeforePopup(url, disposition);
 }
 
