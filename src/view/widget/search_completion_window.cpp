b71ab9d86b9bf96e47ef750d7ef66d389185e81b
xushaohua <xushaohua@deepin.com>
2018-01-16 17:01:08 +0800

    Set search completion window as native window
    Change-Id: I9621d6bae37c1ef42a4f346d9d885be13fdac9a2

--- src/view/widget/search_completion_window.cpp
+++ src/view/widget/search_completion_window.cpp
@@ -173,9 +173,10 @@ void SearchCompletionWindow::initUI() {
   this->setContentsMargins(0, 0, 0, 0);
   this->setMinimumHeight(kItemHeight);
   this->setFixedWidth(262);
-  this->setWindowFlags(Qt::FramelessWindowHint |
-                       Qt::CustomizeWindowHint |
-                       Qt::BypassWindowManagerHint);
+//  this->setWindowFlags(Qt::FramelessWindowHint |
+//                       Qt::CustomizeWindowHint |
+//                       Qt::BypassWindowManagerHint);
+  this->setAttribute(Qt::WA_NativeWindow, true);
 
   ThemeManager::instance()->registerWidget(this);
 }
