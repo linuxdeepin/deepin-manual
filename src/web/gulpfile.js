01f13d2569eb4665b63ca1b8eb9cb75df0f47e7a
myml <wurongjie1@gmail.com>
2018-01-17 14:32:55 +0800

    gulp build add 'DIR' arge
    Change-Id: I32b493ff7daa120eae3e0388f09e76e5c758dbda

--- src/web/gulpfile.js
+++ src/web/gulpfile.js
@@ -1,45 +1,51 @@
-const gulp = require("gulp"),
-	sass = require("gulp-sass"),
-	concat = require("gulp-concat"),
-	rename = require("gulp-rename"),
-	source = require("vinyl-source-stream"),
-	buffer = require("vinyl-buffer"),
-	browserify = require("browserify")
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  source = require('vinyl-source-stream'),
+  buffer = require('vinyl-buffer'),
+  browserify = require('browserify');
 
-const RootDir = "../../PageRoot/www/"
+let RootDir = '../../PageRoot/www/';
 
 //编译js到index.js
-gulp.task("js", () => {
-	// process.env.NODE_ENV = 'production'
-	return browserify("js/App.js")
-		.transform("babelify", { presets: ["env", "react"] })
-		.bundle()
-		.pipe(source("bundle.js"))
-		.pipe(buffer())
-		.pipe(rename("index.js"))
-		.pipe(gulp.dest(RootDir))
-})
+gulp.task('js', () => {
+  return browserify('js/App.js')
+    .transform('babelify', { presets: ['env', 'react'] })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(rename('index.js'))
+    .pipe(gulp.dest(RootDir));
+});
 //编译scss到css/
-gulp.task("sass", () => {
-	return gulp
-		.src("sass/*.scss")
-		.pipe(sass())
-		.pipe(concat("index.css"))
-		.pipe(gulp.dest(RootDir))
-})
+gulp.task('sass', () => {
+  return gulp
+    .src('sass/*.scss')
+    .pipe(sass())
+    .pipe(concat('index.css'))
+    .pipe(gulp.dest(RootDir));
+});
 //移动html文件
-gulp.task("html", () => {
-	return gulp.src("*.html").pipe(gulp.dest(RootDir))
-})
+gulp.task('html', () => {
+  return gulp.src('*.html').pipe(gulp.dest(RootDir));
+});
 
-const tasks = ["js", "sass", "html"]
+const tasks = ['js', 'sass', 'html'];
 
 //编译全部
-gulp.task("build", tasks)
+gulp.task('build', () => {
+  if (process.argv[4]) {
+    RootDir = process.argv[4];
+    gulp.start(tasks);
+  } else {
+    console.error('gulp build -d DIR');
+  }
+});
 
 //调试模式，自动刷新浏览器
-gulp.task("serve", tasks, () => {
-	gulp.watch("js/*.js*", ["js"])
-	gulp.watch("sass/*.scss", ["sass"])
-	gulp.watch("*.html", ["html"])
-})
+gulp.task('serve', tasks, () => {
+  gulp.watch('js/*.js*', ['js']);
+  gulp.watch('sass/*.scss', ['sass']);
+  gulp.watch('*.html', ['html']);
+});
