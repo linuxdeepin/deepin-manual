b7a95621d9123c94ed9673f7564cd8b4b5f26844
myml <wurongjie1@gmail.com>
2018-01-26 15:25:27 +0800

    nodejs update
    Change-Id: I40a3c88adc17f913f24a92ae6f2d2ab7c412d0f9

--- src/web/js/nav.jsx
+++ src/web/js/nav.jsx
@@ -1,75 +1,73 @@
-import React, { Component } from "react"
-import ReactDOM from "react-dom"
-import { Link } from "react-router-dom"
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 
 class Nav extends Component {
-	componentDidMount() {
-		document.getElementById("article").style.marginLeft = ReactDOM.findDOMNode(
-			this
-		).clientWidth
-		this.componentDidUpdate()
-	}
-	componentDidUpdate() {
-		let hashDOM = ReactDOM.findDOMNode(this).querySelector(".hash")
-		if (hashDOM == null) {
-			return
-		}
-		if (hashDOM.getAttribute("cid") == this.props.hlist[0].id) {
-			ReactDOM.findDOMNode(this).scrollTop = 0
-			return
-		}
-		hashDOM.scrollIntoViewIfNeeded(false)
-	}
-	click(e) {
-		let cid = e.target.getAttribute("cid")
-		if (cid) {
-			this.props.setHash(cid)
-		}
-	}
-	wheel(e) {
-		let nav = ReactDOM.findDOMNode(this)
-		if (e.deltaY > 0) {
-			if (
-				e.deltaY > 0 &&
-				nav.scrollHeight == nav.clientHeight + nav.scrollTop
-			) {
-				e.preventDefault()
-			} else if (e.deltaY < 0 && nav.scrollTop == 0) {
-				e.preventDefault()
-			}
-		}
-	}
-	render() {
-		return (
-			<div
-				id="nav"
-				lang={global.lang}
-				onClick={e => this.click(e)}
-				onWheel={this.wheel.bind(this)}
-			>
-				<div id="hlist">
-					<Link to="/index">
-						<div type="h2" id="backHome">
-							{global.i18n["ToIndexPage"]}
-						</div>
-					</Link>
-					{this.props.hlist.map(h => {
-						return (
-							<div
-								key={h.id}
-								cid={h.id}
-								type={h.type}
-								className={this.props.hash == h.id ? "hash" : undefined}
-							>
-								{h.text}
-								<br />
-							</div>
-						)
-					})}
-				</div>
-			</div>
-		)
-	}
+  componentDidMount() {
+    document.getElementById('article').style.marginLeft = ReactDOM.findDOMNode(
+      this
+    ).clientWidth;
+    this.componentDidUpdate();
+  }
+  componentDidUpdate() {
+    let hashDOM = ReactDOM.findDOMNode(this).querySelector('.hash');
+    if (hashDOM == null) {
+      return;
+    }
+    if (hashDOM.getAttribute('cid') == this.props.hlist[0].id) {
+      ReactDOM.findDOMNode(this).scrollTop = 0;
+      return;
+    }
+    hashDOM.scrollIntoViewIfNeeded(false);
+  }
+  click(e) {
+    let cid = e.target.getAttribute('cid');
+    if (cid) {
+      this.props.setHash(cid);
+    }
+  }
+  wheel(e) {
+    let nav = ReactDOM.findDOMNode(this);
+    if (e.deltaY > 0) {
+      if (
+        e.deltaY > 0 &&
+        nav.scrollHeight == nav.clientHeight + nav.scrollTop
+      ) {
+        e.preventDefault();
+      } else if (e.deltaY < 0 && nav.scrollTop == 0) {
+        e.preventDefault();
+      }
+    }
+  }
+  render() {
+    return (
+      <div
+        id="nav"
+        lang={global.lang}
+        onClick={e => this.click(e)}
+        onWheel={this.wheel.bind(this)}
+      >
+        <Link to="/index">
+          <div type="h2" id="backHome">
+            {global.i18n['ToIndexPage']}
+          </div>
+        </Link>
+        {this.props.hlist.map(h => {
+          return (
+            <div
+              key={h.id}
+              cid={h.id}
+              type={h.type}
+              className={this.props.hash == h.id ? 'hash' : undefined}
+            >
+              {h.text}
+              <br />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-export default Nav
+export default Nav;
