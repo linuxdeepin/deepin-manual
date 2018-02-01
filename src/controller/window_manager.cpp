841d4854d737fdebdd2a8bb99b11316bf0682c3e
xushaohua <xushaohua@deepin.com>
2018-02-01 11:49:07 +0800

    WindowManager: Set minimum size of web window
    Change-Id: Ifebaf2758543b5e92aa68bf7cddfaf43cf503114

--- src/controller/window_manager.cpp
+++ src/controller/window_manager.cpp
@@ -30,6 +30,8 @@ namespace {
 
 const int kWinWidth = 1000;
 const int kWinHeight = 715;
+const int kWinMinWidth = 800;
+const int kWinMinHeight = 600;
 const int kWinOffset = 30;
 
 }  // namespace
@@ -54,6 +56,7 @@ void WindowManager::openManual(const QString& app_name) {
   }
   WebWindow* window = new WebWindow(search_manager_);
   window->setAppName(app_name);
+  window->setMinimumSize(kWinMinWidth, kWinMinHeight);
   window->resize(kWinWidth, kWinHeight);
   window->show();
 
