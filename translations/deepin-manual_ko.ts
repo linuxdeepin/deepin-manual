a5f05c108f31b9d44742f047654440536084d358
MarongHappy <36289819+MarongHappy@users.noreply.github.com>
2019-05-05 02:27:53 +0900

    Create deepin-manual_ko.ts

---
+++ translations/deepin-manual_ko.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR" sourcelanguage="en_US">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/app/dman.cpp" line="82"/>
+        <source>Deepin Manual</source>
+        <translation type="unfinished">Deepin 매뉴얼</translation>
+    </message>
+    <message>
+        <location filename="../src/app/dman.cpp" line="83"/>
+        <source>Deepin Manual is designed to help users learn deepin and Deepin applications, providing specific instructions and function descriptions.</source>
+        <translation type="unfinished">Deepin 매뉴얼은 사용자가 Deepin 및 Deepin 응용 프로그램을 학습하는 데 도움이 되도록 설계되어 구체적인 지침과 기능 설명을 제공합니다.</translation>
+    </message>
+    <message>
+        <location filename="../src/view/web_event_delegate.cpp" line="63"/>
+        <source>Copy</source>
+        <translation type="unfinished">복사하기</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="32"/>
+        <source>System</source>
+        <translation type="unfinished">시스템</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="33"/>
+        <source>Applications</source>
+        <translation type="unfinished">응용 프로그램</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="35"/>
+        <source>Sorry, there are no search results of &quot;%1&quot;</source>
+        <translation type="unfinished">죄송합니다. &quot;%1&quot;의 검색 결과가 없습니다</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="36"/>
+        <source>Change your keywords and try again, or search it in Deepin Wiki</source>
+        <translation type="unfinished">키워드를 변경하고 다시 시도하거나 Deepin 위키에서 검색하세요</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="38"/>
+        <source>Home</source>
+        <translation type="unfinished">홈</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="39"/>
+        <source>Deepin Wiki</source>
+        <translation type="unfinished">Deepin 위키</translation>
+    </message>
+    <message>
+        <location filename="../src/view/widget/search_completion_window.cpp" line="113"/>
+        <location filename="../src/view/widget/search_completion_window.cpp" line="162"/>
+        <source>Search &quot;%1&quot; in the full text</source>
+        <translation type="unfinished">전체 텍스트에서 &quot;%1&quot; 검색</translation>
+    </message>
+    <message>
+        <location filename="../src/view/widget/title_bar.cpp" line="96"/>
+        <source>Search</source>
+        <translation type="unfinished">검색</translation>
+    </message>
+</context>
+</TS>
