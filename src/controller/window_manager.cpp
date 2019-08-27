937a32a7942863fc6a9d1b08a2ca63da4856973f
zhiwei.tang <zhiwei.tang@archermind.com>
2019-08-27 11:05:09 +0800

    fix(manual): Modify window corner radius、position and methods
    Set up window radius, application startup location, replace deprecated methods

--- src/controller/window_manager.cpp
+++ src/controller/window_manager.cpp
@@ -27,16 +27,14 @@
 namespace dman {
 
 namespace {
-
 const int kWinWidth = 1024;
 const int kWinHeight = 680;
 const int kWinMinWidth = 800;
 const int kWinMinHeight = 600;
 const int kWinOffset = 30;
-
 }  // namespace
 
-WindowManager::WindowManager(QObject* parent)
+WindowManager::WindowManager(QObject *parent)
     : QObject(parent)
     , windows_()
     , search_manager_(new SearchManager(this))
@@ -45,11 +43,11 @@ WindowManager::WindowManager(QObject* parent)
 
 WindowManager::~WindowManager() {}
 
-void WindowManager::openManual(const QString& app_name)
+void WindowManager::openManual(const QString &app_name)
 {
     qDebug() << Q_FUNC_INFO << app_name;
     if (windows_.contains(app_name)) {
-        WebWindow* window = windows_.value(app_name);
+        WebWindow *window = windows_.value(app_name);
         if (window != nullptr) {
             window->show();
             window->raise();
@@ -61,11 +59,12 @@ void WindowManager::openManual(const QString& app_name)
     // Add a placeholder record.
     windows_.insert(app_name, nullptr);
 
-    WebWindow* window = new WebWindow(search_manager_);
+    WebWindow *window = new WebWindow(search_manager_);
     window->setAppName(app_name);
     windows_.insert(app_name, window);
     window->resize(kWinWidth, kWinHeight);
     window->setMinimumSize(kWinMinWidth, kWinMinHeight);
+    window->setWindowRadius(18);
     window->show();
     window->activateWindow();
 
@@ -79,20 +78,17 @@ QPoint WindowManager::newWindowPosition()
 {
     // If there is no window created, move new window to center of screen.
     // Else stack window one after another.
-    QDesktopWidget* desktop = QApplication::desktop();
+    QDesktopWidget *desktop = QApplication::desktop();
     Q_ASSERT(desktop != nullptr);
     const QRect geometry = desktop->availableGeometry(QCursor::pos());
-    if (windows_.isEmpty()) {
+    if (windows_.isEmpty() || windows_.size() == 1) {
         const QPoint center = geometry.center();
         return QPoint(center.x() - kWinWidth / 2, center.y() - kWinHeight / 2);
-    } else if (windows_.size() == 1) {
-        last_new_window_pos_ = QPoint(0, 0);
-        return last_new_window_pos_;
     } else {
         last_new_window_pos_.setX(last_new_window_pos_.x() + kWinOffset);
         last_new_window_pos_.setY(last_new_window_pos_.y() + kWinOffset);
         if ((last_new_window_pos_.x() + kWinWidth >= geometry.width()) ||
-            (last_new_window_pos_.y() + kWinHeight >= geometry.height())) {
+                (last_new_window_pos_.y() + kWinHeight >= geometry.height())) {
             last_new_window_pos_.setX(0);
             last_new_window_pos_.setY(0);
         }
@@ -100,7 +96,7 @@ QPoint WindowManager::newWindowPosition()
     }
 }
 
-void WindowManager::onWindowClosed(const QString& app_name)
+void WindowManager::onWindowClosed(const QString &app_name)
 {
     windows_.remove(app_name);
 }
--- src/view/web_window.cpp
+++ src/view/web_window.cpp
@@ -48,225 +48,242 @@ const int kSearchDelay = 200;
 
 }  // namespace
 
-WebWindow::WebWindow(SearchManager* search_manager, QWidget* parent)
+WebWindow::WebWindow(SearchManager *search_manager, QWidget *parent)
     : Dtk::Widget::DMainWindow(parent),
       app_name_(),
       search_manager_(search_manager),
       search_proxy_(new SearchProxy(this)),
-      search_timer_() {
+      search_timer_()
+{
 
-  search_timer_.setSingleShot(true);
-  this->initUI();
-  this->initConnections();
+    search_timer_.setSingleShot(true);
+    this->initUI();
+    this->initConnections();
 
-  qApp->installEventFilter(this);
+    qApp->installEventFilter(this);
 }
 
-WebWindow::~WebWindow() {
-  if (completion_window_ != nullptr) {
-    delete completion_window_;
-    completion_window_ = nullptr;
-  }
-}
-
-void WebWindow::setAppName(const QString& app_name) {
-  app_name_ = app_name;
-
-  const QFileInfo info(kIndexPage);
-  web_view_->load(QUrl::fromLocalFile(info.absoluteFilePath()));
-}
-
-void WebWindow::initConnections() {
-  connect(title_bar_, &TitleBar::searchTextChanged,
-          this, &WebWindow::onSearchTextChanged);
-  connect(web_view_->page(), &QCefWebPage::loadFinished,
-          this, &WebWindow::onWebPageLoadFinished);
-  connect(title_bar_, &TitleBar::downKeyPressed,
-          completion_window_, &SearchCompletionWindow::goDown);
-  connect(title_bar_, &TitleBar::enterPressed,
-          this, &WebWindow::onTitleBarEntered);
-  connect(title_bar_, &TitleBar::upKeyPressed,
-          completion_window_, &SearchCompletionWindow::goUp);
-  connect(title_bar_, &TitleBar::focusOut,
-          this, &WebWindow::onSearchEditFocusOut);
-
-  connect(search_manager_, &SearchManager::searchAnchorResult,
-          this, &WebWindow::onSearchAnchorResult);
-  connect(search_manager_, &SearchManager::searchContentResult,
-          search_proxy_, &SearchProxy::onContentResult);
-  connect(search_manager_, &SearchManager::searchContentMismatch,
-          search_proxy_, &SearchProxy::mismatch);
-  connect(completion_window_, &SearchCompletionWindow::resultClicked,
-          this, &WebWindow::onSearchResultClicked);
-  connect(completion_window_, &SearchCompletionWindow::searchButtonClicked,
-          this, &WebWindow::onSearchButtonClicked);
-  connect(&search_timer_, &QTimer::timeout,
-          this, &WebWindow::onSearchTextChangedDelay);
-}
-
-void WebWindow::initUI() {
-  i18n_ = new I18nProxy(this);
-
-  completion_window_ = new SearchCompletionWindow();
-  completion_window_->hide();
-
-  title_bar_ = new TitleBar();
-  title_bar_proxy_ = new TitleBarProxy(title_bar_, this);
-  this->titlebar()->setCustomWidget(title_bar_, Qt::AlignCenter, false);
-  this->titlebar()->setSeparatorVisible(true);
-
-  image_viewer_ = new ImageViewer(this);
-  image_viewer_proxy_ = new ImageViewerProxy(image_viewer_, this);
-
-  manual_proxy_ = new ManualProxy(this);
-
-  web_view_ = new QCefWebView();
-  web_view_->page()->setEventDelegate(new WebEventDelegate(this));
-  this->setCentralWidget(web_view_);
-
-  // Disable web security.
-  web_view_->page()->settings()->setWebSecurity(QCefWebSettings::StateDisabled);
-
-  // Use TitleBarProxy instead.
-  QWebChannel* channel = web_view_->page()->webChannel();
-  web_view_->setAcceptDrops(false);
-  channel->registerObject("i18n", i18n_);
-  channel->registerObject("imageViewer", image_viewer_proxy_);
-  channel->registerObject("manual", manual_proxy_);
-  channel->registerObject("search", search_proxy_);
-  channel->registerObject("titleBar", title_bar_proxy_);
-
-  this->setFocusPolicy(Qt::ClickFocus);
-}
-
-void WebWindow::resizeEvent(QResizeEvent* event) {
-  QWidget::resizeEvent(event);
-  title_bar_->setFixedWidth(event->size().width());
-}
-
-void WebWindow::onSearchEditFocusOut() {
-  QTimer::singleShot(20, [=]() {
-    this->completion_window_->hide();
-  });
-}
-
-void WebWindow::onSearchButtonClicked() {
-  const QString keyword = title_bar_->getSearchText();
-  search_manager_->searchContent(keyword);
-
-  // Show search page.
-  web_view_->page()->runJavaScript(
-      QString("openSearchPage('%1')").arg(keyword));
-}
-
-void WebWindow::onSearchResultClicked(const SearchAnchorResult& result) {
-  web_view_->page()->runJavaScript(
-      QString("open('%1', '%2', '%3')")
-          .arg(result.app_name)
-          .arg(result.anchorId)
-          .arg(result.anchor));
-}
-
-void WebWindow::onSearchTextChanged(const QString& text) {
-  if (text.size() > 1) {
-    search_timer_.stop();
-    search_timer_.start(kSearchDelay);
-  } else {
-    this->onSearchEditFocusOut();
-  }
-}
-
-void WebWindow::onSearchTextChangedDelay() {
-  const QString text = title_bar_->getSearchText();
-  // Filters special chars.
-  if (text.size() <= 1 || text.contains(QRegExp("[+-_$!@#%^&\\(\\)]"))) {
-    return;
-  }
-
-  completion_window_->setKeyword(text);
-
-  // Do real search.
-  search_manager_->searchAnchor(text);
-}
-
-void WebWindow::onTitleBarEntered() {
-  const QString text = title_bar_->getSearchText();
-  if (text.size() > 1) {
-    completion_window_->onEnterPressed();
-  }
-}
-
-void WebWindow::onWebPageLoadFinished(bool ok) {
-  if (ok) {
-    if (app_name_.isEmpty()) {
-      web_view_->page()->runJavaScript("index()");
-    } else {
-      QString real_path(app_name_);
-      if (real_path.contains('/')) {
-        // Open markdown file with absolute path.
-        QFileInfo info(real_path);
-        real_path = info.canonicalFilePath();
-        web_view_->page()->runJavaScript(
-            QString("open('%1')").arg(real_path));
-      } else {
-        // Open system manual.
-        web_view_->page()->runJavaScript(
-            QString("open('%1')").arg(app_name_));
-      }
+WebWindow::~WebWindow()
+{
+    if (completion_window_ != nullptr) {
+        delete completion_window_;
+        completion_window_ = nullptr;
     }
-  }
 }
 
-void WebWindow::onSearchAnchorResult(const QString& keyword,
-                                     const SearchAnchorResultList& result) {
-  // Ignore this signal if current manual window is not present.
-  if (keyword != completion_window_->keyword()) {
-    return;
-  }
+void WebWindow::setAppName(const QString &app_name)
+{
+    app_name_ = app_name;
 
-  Q_UNUSED(keyword);
-  if (result.isEmpty()) {
-    // Hide completion window if no anchor entry matches.
+    const QFileInfo info(kIndexPage);
+    web_view_->load(QUrl::fromLocalFile(info.absoluteFilePath()));
+}
+
+void WebWindow::initConnections()
+{
+    connect(title_bar_, &TitleBar::searchTextChanged,
+            this, &WebWindow::onSearchTextChanged);
+    connect(web_view_->page(), &QCefWebPage::loadFinished,
+            this, &WebWindow::onWebPageLoadFinished);
+    connect(title_bar_, &TitleBar::downKeyPressed,
+            completion_window_, &SearchCompletionWindow::goDown);
+    connect(title_bar_, &TitleBar::enterPressed,
+            this, &WebWindow::onTitleBarEntered);
+    connect(title_bar_, &TitleBar::upKeyPressed,
+            completion_window_, &SearchCompletionWindow::goUp);
+    connect(title_bar_, &TitleBar::focusOut,
+            this, &WebWindow::onSearchEditFocusOut);
+
+    connect(search_manager_, &SearchManager::searchAnchorResult,
+            this, &WebWindow::onSearchAnchorResult);
+    connect(search_manager_, &SearchManager::searchContentResult,
+            search_proxy_, &SearchProxy::onContentResult);
+    connect(search_manager_, &SearchManager::searchContentMismatch,
+            search_proxy_, &SearchProxy::mismatch);
+    connect(completion_window_, &SearchCompletionWindow::resultClicked,
+            this, &WebWindow::onSearchResultClicked);
+    connect(completion_window_, &SearchCompletionWindow::searchButtonClicked,
+            this, &WebWindow::onSearchButtonClicked);
+    connect(&search_timer_, &QTimer::timeout,
+            this, &WebWindow::onSearchTextChangedDelay);
+}
+
+void WebWindow::initUI()
+{
+    i18n_ = new I18nProxy(this);
+
+    completion_window_ = new SearchCompletionWindow();
     completion_window_->hide();
-  } else {
-    completion_window_->show();
-    completion_window_->raise();
-    completion_window_->autoResize();
-    // Move to below of search edit.
-    const QPoint local_point(this->rect().width() / 2 - 94, 36);
-    const QPoint global_point(this->mapToGlobal(local_point));
-    completion_window_->move(global_point);
-    completion_window_->setFocusPolicy(Qt::NoFocus);
-    completion_window_->setFocusPolicy(Qt::StrongFocus);
-    completion_window_->setSearchAnchorResult(result);
-  }
+
+    title_bar_ = new TitleBar();
+    title_bar_proxy_ = new TitleBarProxy(title_bar_, this);
+//    this->titlebar()->setCustomWidget(title_bar_, Qt::AlignCenter, false);
+    this->titlebar()->addWidget(title_bar_, Qt::AlignCenter);
+    this->titlebar()->setSeparatorVisible(true);
+
+    image_viewer_ = new ImageViewer(this);
+    image_viewer_proxy_ = new ImageViewerProxy(image_viewer_, this);
+
+    manual_proxy_ = new ManualProxy(this);
+
+    web_view_ = new QCefWebView();
+    web_view_->page()->setEventDelegate(new WebEventDelegate(this));
+    this->setCentralWidget(web_view_);
+
+    // Disable web security.
+    web_view_->page()->settings()->setWebSecurity(QCefWebSettings::StateDisabled);
+
+    // Use TitleBarProxy instead.
+    QWebChannel *channel = web_view_->page()->webChannel();
+    web_view_->setAcceptDrops(false);
+    channel->registerObject("i18n", i18n_);
+    channel->registerObject("imageViewer", image_viewer_proxy_);
+    channel->registerObject("manual", manual_proxy_);
+    channel->registerObject("search", search_proxy_);
+    channel->registerObject("titleBar", title_bar_proxy_);
+
+    this->setFocusPolicy(Qt::ClickFocus);
 }
 
-bool WebWindow::eventFilter(QObject* watched, QEvent* event) {
-  // Filters mouse press event only.
-  if (event->type() == QEvent::MouseButtonPress &&
-      qApp->activeWindow() == this &&
-      watched->objectName() == QLatin1String("QMainWindowClassWindow")) {
-    QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
-    switch (mouseEvent->button()) {
-      case Qt::BackButton: {
-        title_bar_proxy_->backwardButtonClicked();
-        break;
-      }
-      case Qt::ForwardButton: {
-        title_bar_proxy_->forwardButtonClicked();
-        break;
-      }
-      default: {
-      }
+void WebWindow::resizeEvent(QResizeEvent *event)
+{
+    QWidget::resizeEvent(event);
+    title_bar_->setFixedWidth(event->size().width());
+}
+
+void WebWindow::onSearchEditFocusOut()
+{
+    QTimer::singleShot(20, [ = ]() {
+        this->completion_window_->hide();
+    });
+}
+
+void WebWindow::onSearchButtonClicked()
+{
+    const QString keyword = title_bar_->getSearchText();
+    search_manager_->searchContent(keyword);
+
+    // Show search page.
+    web_view_->page()->runJavaScript(
+        QString("openSearchPage('%1')").arg(keyword));
+}
+
+void WebWindow::onSearchResultClicked(const SearchAnchorResult &result)
+{
+    web_view_->page()->runJavaScript(
+        QString("open('%1', '%2', '%3')")
+        .arg(result.app_name)
+        .arg(result.anchorId)
+        .arg(result.anchor));
+}
+
+void WebWindow::onSearchTextChanged(const QString &text)
+{
+    if (text.size() > 1) {
+        search_timer_.stop();
+        search_timer_.start(kSearchDelay);
+    } else {
+        this->onSearchEditFocusOut();
     }
-  }
-  return QObject::eventFilter(watched, event);
 }
 
-void WebWindow::closeEvent(QCloseEvent* event) {
-  QWidget::closeEvent(event);
-  emit this->closed(app_name_);
+void WebWindow::onSearchTextChangedDelay()
+{
+    const QString text = title_bar_->getSearchText();
+    // Filters special chars.
+    if (text.size() <= 1 || text.contains(QRegExp("[+-_$!@#%^&\\(\\)]"))) {
+        return;
+    }
+
+    completion_window_->setKeyword(text);
+
+    // Do real search.
+    search_manager_->searchAnchor(text);
+}
+
+void WebWindow::onTitleBarEntered()
+{
+    const QString text = title_bar_->getSearchText();
+    if (text.size() > 1) {
+        completion_window_->onEnterPressed();
+    }
+}
+
+void WebWindow::onWebPageLoadFinished(bool ok)
+{
+    if (ok) {
+        if (app_name_.isEmpty()) {
+            web_view_->page()->runJavaScript("index()");
+        } else {
+            QString real_path(app_name_);
+            if (real_path.contains('/')) {
+                // Open markdown file with absolute path.
+                QFileInfo info(real_path);
+                real_path = info.canonicalFilePath();
+                web_view_->page()->runJavaScript(
+                    QString("open('%1')").arg(real_path));
+            } else {
+                // Open system manual.
+                web_view_->page()->runJavaScript(
+                    QString("open('%1')").arg(app_name_));
+            }
+        }
+    }
+}
+
+void WebWindow::onSearchAnchorResult(const QString &keyword,
+                                     const SearchAnchorResultList &result)
+{
+    // Ignore this signal if current manual window is not present.
+    if (keyword != completion_window_->keyword()) {
+        return;
+    }
+
+    Q_UNUSED(keyword);
+    if (result.isEmpty()) {
+        // Hide completion window if no anchor entry matches.
+        completion_window_->hide();
+    } else {
+        completion_window_->show();
+        completion_window_->raise();
+        completion_window_->autoResize();
+        // Move to below of search edit.
+        const QPoint local_point(this->rect().width() / 2 - 94, 36);
+        const QPoint global_point(this->mapToGlobal(local_point));
+        completion_window_->move(global_point);
+        completion_window_->setFocusPolicy(Qt::NoFocus);
+        completion_window_->setFocusPolicy(Qt::StrongFocus);
+        completion_window_->setSearchAnchorResult(result);
+    }
+}
+
+bool WebWindow::eventFilter(QObject *watched, QEvent *event)
+{
+    // Filters mouse press event only.
+    if (event->type() == QEvent::MouseButtonPress &&
+            qApp->activeWindow() == this &&
+            watched->objectName() == QLatin1String("QMainWindowClassWindow")) {
+        QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+        switch (mouseEvent->button()) {
+        case Qt::BackButton: {
+            title_bar_proxy_->backwardButtonClicked();
+            break;
+        }
+        case Qt::ForwardButton: {
+            title_bar_proxy_->forwardButtonClicked();
+            break;
+        }
+        default: {
+        }
+        }
+    }
+    return QObject::eventFilter(watched, event);
+}
+
+void WebWindow::closeEvent(QCloseEvent *event)
+{
+    QWidget::closeEvent(event);
+    emit this->closed(app_name_);
 }
 
 }  // namespace dman
