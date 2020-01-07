4a35193343158faf70bb8056871b6a00291f29f7
wuJian <jian.wu@deepin.io>
2020-01-07 15:01:02 +0800

    chore: add func of auto create all db-file for all system

--- src/app/generate_search_db.cpp
+++ src/app/generate_search_db.cpp
@@ -41,86 +41,89 @@ int main(int argc, char **argv)
 
     QDir::setCurrent(DMAN_SEARCH_WORK_DIR);
 
-    dman::SearchDb db;
-    db.initDb(DMAN_SEARCH_ORIG_DB);
-    db.initSearchTable();
+    for (const QString &dbType :
+         QDir(DMAN_ORIG_MANUAL_DIR).entryList(QDir::NoDotAndDotDot | QDir::Dirs)) {
+        QString strCreateDbPath = DMAN_SEARCH_CREATE_DB_PATH;
+        strCreateDbPath += "/" + dbType;
 
-    for (const QString &locale : {
-                "zh_CN", "en_US"
-            }) {
-        QString strManualDir = DMAN_ORIG_MANUAL_DIR;
-        int nType = Dtk::Core::DSysInfo::deepinType();
-        if (Dtk::Core::DSysInfo::DeepinServer == (Dtk::Core::DSysInfo::DeepinType)nType) {
-            //   strManualDir += "/professional";
-            strManualDir += "/server";
-        } else {
-            // strManualDir += "/server";
-            strManualDir += "/professional";
+        QDir dir(strCreateDbPath);
+        if (!dir.exists()) {
+            dir.mkpath(strCreateDbPath);
         }
+        strCreateDbPath += "/search.db";
 
-        for (const QString &app_name :
-                QDir(strManualDir).entryList(QDir::NoDotAndDotDot | QDir::Dirs)) {
-            const QString md_file =
-                QStringList {strManualDir, app_name, locale, "index.md"}.join(QDir::separator());
-            if (!QFileInfo(md_file).isFile()) {
-                qWarning() << md_file << "not found";
-                continue;
-            }
+        dman::SearchDb db;
+        db.initDb(strCreateDbPath);
+        db.initSearchTable();
 
-            QDir manualDir = QDir(DMAN_ORIG_MANUAL_DIR);
-            manualDir.cdUp();
-            qDebug() << manualDir.path();
-            QString searchIndexFilePath = QString("%1/%2/%3/%4")
-                                          .arg(manualDir.path())
-                                          .arg("src")
-                                          .arg("web")
-                                          .arg("toSearchIndex.js");
-            qDebug() << searchIndexFilePath;
-            QString out, err;
-            //            QStringList cmdList = {"node"};
-            //            const bool queryNode = dman::SpawnCmd("which", cmdList);
-            //            if (!queryNode) {
-            //                qWarning() << err << queryNode << md_file;
-            //            }
-
-            const QStringList cmd = {searchIndexFilePath, "-f", md_file};
-            const bool ok = dman::SpawnCmd("/usr/bin/node", cmd, out, err);
-            if (!ok) {
-                qWarning() << err << ok << md_file;
-                continue;
-            }
-
-            // Parse json
-            QJsonDocument document = QJsonDocument::fromJson(out.toLocal8Bit());
-            if (!document.isArray()) {
-                qWarning() << md_file << "Invalid json object:" << out;
-                continue;
-            }
-
-            const QJsonArray array = document.array();
-            QStringList anchors;
-            QStringList anchorIdList;
-            QStringList contents;
-            bool invalid_entry = false;
-            for (const QJsonValue &value : array) {
-                if (!value.isArray()) {
-                    invalid_entry = true;
-                    qWarning() << md_file << "is not an array:" << value;
-                    break;
+        for (const QString &locale : {"zh_CN", "en_US"}) {
+            QString strManualDir = DMAN_ORIG_MANUAL_DIR;
+            strManualDir += "/" + dbType;
+            for (const QString &app_name :
+                 QDir(strManualDir).entryList(QDir::NoDotAndDotDot | QDir::Dirs)) {
+                const QString md_file =
+                    QStringList {strManualDir, app_name, locale, "index.md"}.join(
+                        QDir::separator());
+                if (!QFileInfo(md_file).isFile()) {
+                    qWarning() << md_file << "not found";
+                    continue;
                 }
 
-                const QJsonArray anchor = value.toArray();
-                const QString id = anchor.at(0).toString();
-                anchorIdList.append(id);
-                const QString title = anchor.at(1).toString();
-                anchors.append(title);
-                const QString content = anchor.at(2).toString();
-                contents.append(content);
-            }
+                QDir manualDir = QDir(DMAN_ORIG_MANUAL_DIR);
+                manualDir.cdUp();
+                qDebug() << manualDir.path();
+                QString searchIndexFilePath = QString("%1/%2/%3/%4")
+                                                  .arg(manualDir.path())
+                                                  .arg("src")
+                                                  .arg("web")
+                                                  .arg("toSearchIndex.js");
+                qDebug() << searchIndexFilePath;
+                QString out, err;
+                //            QStringList cmdList = {"node"};
+                //            const bool queryNode = dman::SpawnCmd("which", cmdList);
+                //            if (!queryNode) {
+                //                qWarning() << err << queryNode << md_file;
+                //            }
 
-            if (!invalid_entry) {
-                qDebug() << "add search entry" << app_name << locale << anchors << endl;
-                db.addSearchEntry(app_name, locale, anchors, anchorIdList, contents);
+                const QStringList cmd = {searchIndexFilePath, "-f", md_file};
+                const bool ok = dman::SpawnCmd("/usr/bin/node", cmd, out, err);
+                if (!ok) {
+                    qWarning() << err << ok << md_file;
+                    continue;
+                }
+
+                // Parse json
+                QJsonDocument document = QJsonDocument::fromJson(out.toLocal8Bit());
+                if (!document.isArray()) {
+                    qWarning() << md_file << "Invalid json object:" << out;
+                    continue;
+                }
+
+                const QJsonArray array = document.array();
+                QStringList anchors;
+                QStringList anchorIdList;
+                QStringList contents;
+                bool invalid_entry = false;
+                for (const QJsonValue &value : array) {
+                    if (!value.isArray()) {
+                        invalid_entry = true;
+                        qWarning() << md_file << "is not an array:" << value;
+                        break;
+                    }
+
+                    const QJsonArray anchor = value.toArray();
+                    const QString id = anchor.at(0).toString();
+                    anchorIdList.append(id);
+                    const QString title = anchor.at(1).toString();
+                    anchors.append(title);
+                    const QString content = anchor.at(2).toString();
+                    contents.append(content);
+                }
+
+                if (!invalid_entry) {
+                    qDebug() << "add search entry" << app_name << locale << anchors << endl;
+                    db.addSearchEntry(app_name, locale, anchors, anchorIdList, contents);
+                }
             }
         }
     }
