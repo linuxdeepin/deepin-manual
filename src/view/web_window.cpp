810a35af51d4f244000f63f5b141f0f8269769c7
wangmingliang <wangmingliang@uniontech.com>
2020-07-27 15:21:24 +0800

    fix:修复搜索框没有右键菜单问题
    Description: 添加CustomContextMenu属性
    
    Log: null
    
    link: null

--- src/view/web_window.cpp
+++ src/view/web_window.cpp
@@ -271,6 +271,7 @@ void WebWindow::initUI()
     search_edit_->setFixedSize(350, 44);
     search_edit_->setPlaceHolder(QObject::tr("Search"));
     search_edit_->setFocus();
+    search_edit_->setContextMenuPolicy(Qt::CustomContextMenu);
 
     if (Utils::hasSelperSupport()) {
         DMenu *pMenu = new DMenu;
