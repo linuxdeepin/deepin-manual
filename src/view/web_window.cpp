b509d4780fcad383d76d384122c0e9b711c62ebb
wujian <wujian@uniontech.com>
2020-12-18 10:55:50 +0800

    fix:bug-57879,修复wayland下，标题栏会出现黑方块问题。
     wayland下， 窗口焦点在qwebengines上时，如果titlebar原有没有buttonbox，而后将其show出后， 需再次刷新主窗口
    
    Log: 打开帮助手册 选中查看某一应用的介绍，点击后发现 右上角选项被黑色区域
    
    Bug: https://pms.uniontech.com/zentao/bug-view-57879.html
    Change-Id: Ic1cbd0e96a5f7607cbdeae000a8be3b225dd0df6

--- src/view/web_window.cpp
+++ src/view/web_window.cpp
@@ -144,6 +144,9 @@ void WebWindow::updateBtnBox()
 {
     if (m_forwardButton->isEnabled() || m_backButton->isEnabled()) {
         buttonBox->show();
+        //wayland下， 窗口焦点在qwebengines上时，如果titlebar原有没有buttonbox，将titlebar上buttonbox->show（）后会出现黑方角，像是没有渲染完成一样。。。。
+        //避免方法：刷新一次主窗口。。
+        this->update();
     } else {
         buttonBox->hide();
     }
@@ -238,7 +241,7 @@ void WebWindow::closeEvent(QCloseEvent *event)
     //保存窗口大小
     saveWindowSize();
 
-    if (QOpenGLContext *context = QOpenGLContext::currentContext()){
+    if (QOpenGLContext *context = QOpenGLContext::currentContext()) {
         context->doneCurrent();
     }
     deleteLater();
@@ -397,7 +400,7 @@ void WebWindow::initConnections()
     connect(search_edit_, &SearchEdit::enterPressed, this, &WebWindow::onTitleBarEntered);
     connect(search_edit_, &SearchEdit::upKeyPressed, completion_window_,
             &SearchCompletionWindow::goUp);
-   connect(search_edit_, &SearchEdit::focusChanged, this, &WebWindow::onSearchEditFocusOut);
+    connect(search_edit_, &SearchEdit::focusChanged, this, &WebWindow::onSearchEditFocusOut);
     connect(completion_window_, &SearchCompletionWindow::resultClicked, this,
             &WebWindow::onSearchResultClicked);
     connect(completion_window_, &SearchCompletionWindow::searchButtonClicked, this,
@@ -556,8 +559,8 @@ void WebWindow::initWebView()
             theme_proxy_, &ThemeProxy::slot_ThemeChange);
     //应用启动时，页面加载成功时间获取
     connect(manual_proxy_, &ManualProxy::startFinish, this, &WebWindow::manualStartFinish);
-   connect(DGuiApplicationHelper::instance(), &DGuiApplicationHelper::themeTypeChanged,
-           this, &WebWindow::slot_ThemeChanged);
+    connect(DGuiApplicationHelper::instance(), &DGuiApplicationHelper::themeTypeChanged,
+            this, &WebWindow::slot_ThemeChanged);
 
     manual_proxy_->setApplicationState("dde");
 }
