686debe81ee97965c875a365d8e15b04b707e28d
wangyou <wangyou@uniontech.com>
2021-09-07 15:14:58 +0800

    fix: 修复盘古W部分图片显示异常问题
    1. 禁用程序启动运行环境变量single-process
    2. 恢复上次修改图片为原始图片
    
    Log: 修复盘古W部分图片显示异常问题
    
    Bug: https://pms.uniontech.com/zentao/bug-view-91354.html
    
    Change-Id: Ib44930ce16cf739877fadf52a6640eb3c2d0f675

--- src/app/dman.cpp
+++ src/app/dman.cpp
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
     qputenv("DXCB_FAKE_PLATFORM_NAME_XCB", "true");
     //禁用GPU
     qputenv("QTWEBENGINE_CHROMIUM_FLAGS", "--disable-gpu");
-    qputenv("QTWEBENGINE_CHROMIUM_FLAGS", "--single-process");
+    //qputenv("QTWEBENGINE_CHROMIUM_FLAGS", "--single-process");
     //所有进程类型禁用沙箱..此配置开启禁用gpu后无效
 //    qputenv("QTWEBENGINE_CHROMIUM_FLAGS", "--no-sandbox");
     //龙芯机器配置,使得DApplication能正确加载QTWEBENGINE
