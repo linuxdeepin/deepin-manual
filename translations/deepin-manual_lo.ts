8b63ea255fbe87cecc7aa35e6291c0953b9d341b
dengzhongyuan365-dev <dengzhongyuan@uniontech.com>
2025-08-07 19:53:30 +0800

    chore: Add new Lao translation file (#569)
    * chore: Add new Lao translation file
    
    - Add new Lao translation file
    
    Log: Improve language support by adding Lao translation
    
    * chore: Update version to 6.5.37
    
     - update version to 6.5.37
    
     log: update version to 6.5.37

---
+++ translations/deepin-manual_lo.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lo">
+<context>
+    <name>ManualProxy</name>
+    <message>
+        <location filename="../src/view/manual_proxy.cpp" line="363"/>
+        <source>Learn Basic Operations</source>
+        <translation>ຮຽນຮູ້ການດຳເນີນງານພື້ນຖານ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/manual_proxy.cpp" line="366"/>
+        <source>Common Application Libraries</source>
+        <translation>ຫ້ອງສະໝຸດແອັບພິເຄຊັນທົ່ວໄປ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/manual_proxy.cpp" line="369"/>
+        <source>Desktop Environment</source>
+        <translation>ສະແດງຫນ້າທີ່ສະແດງການດຳເນີນງານພື້ນຖານ</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/app/dman.cpp" line="136"/>
+        <source>Manual</source>
+        <translation>ຮຽນຮູ້</translation>
+    </message>
+    <message>
+        <location filename="../src/app/dman.cpp" line="137"/>
+        <source>Manual is designed to help users learn the operating system and its applications, providing specific instructions and function descriptions.</source>
+        <translation>ຄູ່ມືນີ້ຖືກອອກແບບເພື່ອຊ່ວຍໃຫ້ຜູ້ໃຊ້ຮຽນຮູ້ກ່ຽວກັບລະບົບປະຕິບັດງານ ແລະ ແອັບພິເຄຊັນຕ່າງໆ ໂດຍມີຄຳແນະນຳທີ່ຈະເຮັດຕາມ ແລະ ຄຳອະທິບາຍການໃຊ້ງານຂອງແຕ່ລະຟັງຊັນ.</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="25"/>
+        <source>Quick Start</source>
+        <translation>ການເລີ່ມຕົ້ນທີ່ດີ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="26"/>
+        <source>Video Guide</source>
+        <translation>ວິດີໂອຄູ່ມື</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="27"/>
+        <source>System</source>
+        <translation>ລະບົບ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="28"/>
+        <source>Applications</source>
+        <translation>ແອັບພິເຄຊັນ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="29"/>
+        <source>No search results</source>
+        <translation>ບໍ່ພົບຜົນຄົ້ນຫາ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="30"/>
+        <source>Home</source>
+        <translation>ຫນ້າຫລັກ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="31"/>
+        <source>  result</source>
+        <translation>ຜົນຄົ້ນຫາ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="32"/>
+        <source>  results</source>
+        <translation>ຜົນຄົ້ນຫາ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="33"/>
+        <source>Click to view </source>
+        <translation>ຄລິກເພື່ອເບິ່ງ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="34"/>
+        <source>View all</source>
+        <translation>ເບິ່ງທັງໝົດ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="36"/>
+        <source>App Store</source>
+        <translation>ຮ້ານໂປລີ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/web_window.cpp" line="767"/>
+        <source>Search</source>
+        <translation>ຄົ້ນຫາ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/i18n_proxy.cpp" line="35"/>
+        <source>Support</source>
+        <translation>ຊ່ວຍເຫຼືອ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/web_window.cpp" line="363"/>
+        <source>The content was updated</source>
+        <translation>ເນື້ອໃນໄດ້ຖືກອັບເດດ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/web_window.cpp" line="1038"/>
+        <source>Copy</source>
+        <translation>ຄັດລອກ</translation>
+    </message>
+    <message>
+        <location filename="../src/view/widget/search_completion_window.cpp" line="197"/>
+        <source>Search for &quot;%1&quot; in the full text</source>
+        <translation>ຄົ້ນຫາ &quot;%1&quot; ໃນເນື້ອໃນທັງໝົດ</translation>
+    </message>
+</context>
+<context>
+    <name>WebWindow</name>
+    <message>
+        <location filename="../src/view/web_window.cpp" line="952"/>
+        <source>Ctrl+Alt+F</source>
+        <translation>Ctrl+Alt+F</translation>
+    </message>
+    <message>
+        <location filename="../src/view/web_window.cpp" line="969"/>
+        <source>Ctrl+F</source>
+        <translation>Ctrl+F</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/desktop/desktop_lo.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="Desktop Entry]Name" line="0"/>
+        <source>Deepin Manual</source>
+        <translation>ຄຳສັ່ງການໃຊ້ງານ Deepin</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]GenericName" line="0"/>
+        <source>Manual</source>
+        <translation>ຄຳສັ່ງການໃຊ້ງານ</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]Keywords" line="0"/>
+        <source>documentation;information;manual;help;</source>
+        <translation>ບົດບັນທຶກ;ຂໍ້ມູນ;ຄຳສັ່ງການໃຊ້ງານ;ຄຳແນະນຳ;</translation>
+    </message>
+</context>
+</TS>
